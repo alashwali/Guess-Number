a44afde4348de0599c521c77c71a07add0f6501f
asma laptop <asmaalashwali@hotmail.com>
2023-07-31 02:42:11 +0300

    score = 5;

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ document.querySelector(".check").addEventListener("click", function () {
 });
 //---------------------button again------------------
 document.querySelector(".again").addEventListener("click", function () {
-  score = 20;
+  score = 5;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
   displayMessage("Start guessing...");
